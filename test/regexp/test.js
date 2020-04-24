8519449bb18482314ea79c924eddc9b182181b5e
yuanqing <desk@yuanqing.sg>
2020-04-24 19:53:58 +0800

    Add test for statefulness with `/g` flag

--- test/regexp/test.js
+++ test/regexp/test.js
@@ -1,9 +1,23 @@
 const test = require('ava')
 
 test('no matches', function (t) {
+  t.plan(1)
   t.is(/y/.test('xx'), false)
 })
 
 test('match', function (t) {
+  t.plan(1)
   t.is(/x/.test('xx'), true)
+})
+
+test('stateful with the `g` flag', function (t) {
+  t.plan(7)
+  const regexp = /x/g
+  t.is(regexp.lastIndex, 0)
+  t.is(regexp.test('xx'), true)
+  t.is(regexp.lastIndex, 1)
+  t.is(regexp.test('xx'), true)
+  t.is(regexp.lastIndex, 2)
+  t.is(regexp.test('xx'), false)
+  t.is(regexp.lastIndex, 0)
 })
