69c36073f218fc62f24a242a7507dfc3e322eceb
yuanqing <desk@yuanqing.sg>
2020-08-14 16:19:37 +0800

    Edit tests

--- test/string/match-all.js
+++ test/string/match-all.js
@@ -4,8 +4,8 @@ test('no matches', function (t) {
   t.plan(1)
   const iterator = 'xx'.matchAll(/y/g)
   const result = []
-  for (const match of iterator) {
-    result.push(match[0])
+  for (const matches of iterator) {
+    result.push([matches[0], matches.length, matches.index])
   }
   t.deepEqual(result, [])
 })
@@ -14,21 +14,24 @@ test('match', function (t) {
   t.plan(1)
   const iterator = 'xx'.matchAll(/x/g)
   const result = []
-  for (const match of iterator) {
-    result.push(match[0])
+  for (const matches of iterator) {
+    result.push([matches[0], matches.length, matches.index])
   }
-  t.deepEqual(result, ['x', 'x'])
+  t.deepEqual(result, [
+    ['x', 1, 0],
+    ['x', 1, 1]
+  ])
 })
 
 test('match, with capturing group', function (t) {
   t.plan(1)
   const iterator = 'xyxy'.matchAll(/x(y)/g)
   const result = []
-  for (const match of iterator) {
-    result.push([match[0], match[1]])
+  for (const matches of iterator) {
+    result.push([matches[0], matches[1], matches.length, matches.index])
   }
   t.deepEqual(result, [
-    ['xy', 'y'],
-    ['xy', 'y']
+    ['xy', 'y', 2, 0],
+    ['xy', 'y', 2, 2]
   ])
 })
